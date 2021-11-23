a05ac01dc6e774a4988856a4bc25fa05f855460c
emma <emma.ronaldo7@gmail.com>
2021-11-22 19:15:21 -0500

    done

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('pokemons/', views.pokemons_index, name='pokemons_index'),
     # localhost:8000
@@ -17,5 +18,5 @@ urlpatterns = [
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
     path('pokemons/<int:pokemon_id>/assoc_toy/<int:toy_id>/',views.assoc_toy, name='assoc_toy'),
-
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,92 +1,114 @@
 from django.shortcuts import redirect, render
-
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-from .models import Pokemon, Toy 
-
+from .models import Pokemon, Toy
 from .forms import FeedingForm
-
 from django.views.generic import ListView, DetailView
-
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Define the home view
-
-
-
-def home(request):
-  return render(request, 'home.html')
 
 
 def about(request):
     return render(request, 'about.html')
 
 
+@login_required
 def pokemons_index(request):
-  pokemons = Pokemon.objects.all()
-  return render(request, 'pokemons/index.html', { 'pokemons': pokemons })
+    pokemons = Pokemon.objects.filter(user=request.user)
+    # pokemons = Pokemon.objects.all()
+    return render(request, 'pokemons/index.html', {'pokemons': pokemons})
 
 
-
+@login_required
 def pokemons_detail(request, pokemon_id):
-  pokemon = Pokemon.objects.get(id = pokemon_id)
-  toys_pokemon_doesnt_have = Toy.objects.exclude(
-      id__in=pokemon.toys.all().values_list('id'))
-  feeding_form = FeedingForm()
-  return render(request, 'pokemons/detail.html', {'pokemon': pokemon, 'feeding_form': feeding_form, 'toys': toys_pokemon_doesnt_have})
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    toys_pokemon_doesnt_have = Toy.objects.exclude(
+        id__in=pokemon.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'pokemons/detail.html', {'pokemon': pokemon, 'feeding_form': feeding_form, 'toys': toys_pokemon_doesnt_have})
 
 
-
-class PokemonCreate(CreateView):
-  model = Pokemon
-  fields = ['name', 'ability', 'description', 'height']
-  success_url = '/pokemons/'
-
-
-class PokemonUpdate(UpdateView):
-  model = Pokemon
-  
-  fields = ['ability', 'description', 'height']
-
-
-class PokemonDelete(DeleteView):
-  model = Pokemon
-  success_url = '/pokemons/'
-
-
+@login_required
 def add_feeding(request, pokemon_id):
-  form = FeedingForm(request.POST)
-  if form.is_valid():
-    new_feeding = form.save(commit=False)
-    new_feeding.pokemon_id = pokemon_id
-    new_feeding.save()
-  return redirect('pokemons_detail', pokemon_id=pokemon_id)
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.pokemon_id = pokemon_id
+        new_feeding.save()
+    return redirect('pokemons_detail', pokemon_id=pokemon_id)
 
 
-class ToyCreate(CreateView):
-  model = Toy
-  fields = '__all__'
-
-
-class ToyList(ListView):
-  model = Toy
-
-
-class ToyDetail(DetailView):
-  model = Toy
-
-
-class ToyUpdate(UpdateView):
-  model = Toy
-  fields = ['name', 'color']
-
-
-class ToyDelete(DeleteView):
-  model = Toy
-  success_url = '/toys/'
-
-
+@login_required
 def assoc_toy(request, pokemon_id, toy_id):
 
-  Pokemon.objects.get(id=pokemon_id).toys.add(toy_id)
-  return redirect('pokemons_detail', pokemon_id=pokemon_id)
+    Pokemon.objects.get(id=pokemon_id).toys.add(toy_id)
+    return redirect('pokemons_detail', pokemon_id=pokemon_id)
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('cats_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
+class Home(LoginView):
+  template_name = 'home.html'
+
+
+class PokemonCreate(LoginRequiredMixin, CreateView):
+    model = Pokemon
+    fields = ['name', 'ability', 'description', 'height']
+
+    def form_valid(self, form):
+      form.instance.user = self.request.user  
+      return super().form_valid(form)
+
+
+class PokemonUpdate(LoginRequiredMixin, UpdateView):
+    model = Pokemon
+
+    fields = ['ability', 'description', 'height']
+
+
+class PokemonDelete(LoginRequiredMixin, DeleteView):
+    model = Pokemon
+    success_url = '/pokemons/'
+
+
+class ToyCreate(LoginRequiredMixin, CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyList(LoginRequiredMixin, ListView):
+    model = Toy
+
+
+class ToyDetail(LoginRequiredMixin, DetailView):
+    model = Toy
+
+
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(LoginRequiredMixin, DeleteView):
+    model = Toy
+    success_url = '/toys/'
